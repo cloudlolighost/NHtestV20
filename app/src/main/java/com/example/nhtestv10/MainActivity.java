d573562a4e0c04cce0bd11496d195ee4323f51ba
fcu-d0381912 <jj1029384756@livemail.tw>
2019-05-26 16:10:46 +0800

    V2.1

--- app/src/main/java/com/example/nhtestv10/MainActivity.java
+++ app/src/main/java/com/example/nhtestv10/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this , List.class);
+                intent.setClass(MainActivity.this , ClockRecyclerView.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/nhtestv10/QRcode.java
+++ app/src/main/java/com/example/nhtestv10/QRcode.java
@@ -1,14 +1,22 @@
 package com.example.nhtestv10;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 public class QRcode extends AppCompatActivity {
+    private Button scan_btn;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -32,5 +40,63 @@ public class QRcode extends AppCompatActivity {
         setContentView(R.layout.activity_qrcode);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        scan_btn = (Button)findViewById(R.id.button);
+        final Activity activity = this;
+        scan_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+               /* int ori = getRequestedOrientation();
+                if (ori==-1){
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                }
+                else if (ori == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT){
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                }
+                else {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+
+                }*/
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                //integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
     }
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result!= null)
+        {
+            if (result.getContents()==null)
+            {
+                Toast.makeText(this, "您已取消掃描藥物資訊", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                //Toast.makeText(this,result.getContents(),Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(result.getContents()));
+                startActivity(i);
+            }
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+
+    }
+    }
+
