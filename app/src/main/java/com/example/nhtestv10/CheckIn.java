0bd63b1176b5201c2d06d2893e18b75692bf8956
fcu-d0381912 <jj1029384756@livemail.tw>
2019-05-29 10:11:29 +0800

    V2.2

--- app/src/main/java/com/example/nhtestv10/CheckIn.java
+++ app/src/main/java/com/example/nhtestv10/CheckIn.java
@@ -7,6 +7,12 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class CheckIn extends AppCompatActivity {
     @Override
@@ -30,7 +36,36 @@ public class CheckIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
+        initDate();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+
+    private List<CheckBean> checkBeanList;
+    private MyAdapter mAdapter;
+    private GridView mGridview;
+
+
+    private void initDate() {
+
+        Calendar calendar = Calendar.getInstance(Locale.CHINA);
+        int day = calendar.getActualMaximum(Calendar.DATE); // 获取当前月的天数
+
+        checkBeanList = new ArrayList<CheckBean>();
+        for (int i = 0; i < day + 1; i++) {
+            CheckBean checkBean = new CheckBean();
+            checkBean.day = i;
+            checkBean.check_status = CheckBean.CHECK_NO;
+
+
+            checkBeanList.add(checkBean);
+        }
+
+        mAdapter = new MyAdapter(CheckIn.this);
+        mAdapter.setListDate(checkBeanList);
+
+        mGridview = (GridView) findViewById(R.id.main_gridview);
+        mGridview.setAdapter(mAdapter);
     }
 }
